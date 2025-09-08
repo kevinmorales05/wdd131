6aec907026529a183b8b78b847eaf591db0cd884
u548742577 <kdmorale@pichincha.com>
2025-09-08 08:03:10 -0600

    basic structure

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+
+const currentYear = new Date().getFullYear();
+document.querySelector("#year").textContent = currentYear;
+
+
+const lastModified = document.lastModified;
+document.querySelector("#lastModified").textContent = lastModified;
